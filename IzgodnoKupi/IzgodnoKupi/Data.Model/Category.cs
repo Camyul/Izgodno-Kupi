3de724652807da77a5d858f5f6147d2aac7725da
Camyul <vankata.lt@outlook.com>
2017-11-17 15:31:10 +0200

    Validation Constants and Category Model added

---
+++ IzgodnoKupi/IzgodnoKupi/Data.Model/Category.cs
@@ -0,0 +1,59 @@
+﻿using IzgodnoKupi.Common;
+using IzgodnoKupi.Data.Model.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace IzgodnoKupi.Data.Model
+{
+    public class Category : DataModel
+    {
+        private ICollection<Product> products;
+        private ICollection<Category> subCategories;
+
+        public Category()
+        {
+            this.Products = new HashSet<Product>();
+            this.SubCategories = new HashSet<Category>();
+        }
+
+
+        [Required]
+        //[Index(IsUnique = true)]
+        [MinLength(ValidationConstants.StandardMinLength, ErrorMessage = ValidationConstants.MinLengthFieldErrorMessage)]
+        [MaxLength(ValidationConstants.StandartMaxLength, ErrorMessage = ValidationConstants.MaxLengthFieldErrorMessage)]
+        [RegularExpression(ValidationConstants.EnBgDigitSpaceMinus, ErrorMessage = ValidationConstants.NotAllowedSymbolsErrorMessage)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products
+        {
+            get
+            {
+                return this.products;
+            }
+
+            set
+            {
+                this.products = value;
+            }
+        }
+
+        public bool ShowOnHomePage { get; set; }
+
+        public Guid? ParentCategoryId { get; set; }
+
+
+        public virtual ICollection<Category> SubCategories
+        {
+            get
+            {
+                return this.subCategories;
+            }
+
+            set
+            {
+                this.subCategories = value;
+            }
+        }
+    }
+}
