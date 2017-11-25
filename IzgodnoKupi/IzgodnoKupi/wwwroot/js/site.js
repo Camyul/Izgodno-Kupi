53a38a45976e5070e9315a6d1ef779f9e06a3125
Camyul <vankata.lt@outlook.com>
2017-11-25 18:09:33 +0200

    Add carousel to home page, but jquery doesn't work on cloud

--- IzgodnoKupi/IzgodnoKupi/wwwroot/js/site.js
+++ IzgodnoKupi/IzgodnoKupi/wwwroot/js/site.js
@@ -1,1 +1,17 @@
 ﻿// Write your JavaScript code.
+
+//For Carousel in Home Page
+$(document).ready(function () {
+    $("#slider").responsiveSlides({
+        auto: true,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        pager: false,
+    });
+});
+
+// For Product Menu
+$(document).ready(function () {
+    $(".memenu").memenu();
+});
