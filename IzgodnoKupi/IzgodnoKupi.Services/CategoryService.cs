420967580fce6b878362dfae31a7b094836a7ea6
Camyul <vankata.lt@outlook.com>
2017-11-27 00:36:49 +0200

    Create CategoryService

---
+++ IzgodnoKupi/IzgodnoKupi.Services/CategoryService.cs
@@ -0,0 +1,56 @@
+﻿using Bytes2you.Validation;
+using IzgodnoKupi.Data.Contracts;
+using IzgodnoKupi.Data.Model;
+using IzgodnoKupi.Services.Contracts;
+using System;
+using System.Linq;
+
+namespace IzgodnoKupi.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ISaveContext context;
+        private readonly IEfRepository<Category> categoriesRepo;
+
+        public CategoryService(IEfRepository<Category> categoriesRepo, ISaveContext context)
+        {
+            Guard.WhenArgument(categoriesRepo, "categoriesRepo").IsNull().Throw();
+            Guard.WhenArgument(context, "context").IsNull().Throw();
+
+            this.categoriesRepo = categoriesRepo;
+            this.context = context;
+        }
+
+        public IQueryable<Category> GetAllCategoriesSortedByName()
+        {
+            var result = this.categoriesRepo.All
+                                 .OrderBy(x => x.Name);
+
+            return result;
+        }
+
+        public Category GetById(Guid? id)
+        {
+            Category result = null;
+            if (id.HasValue)
+            {
+                Category product = this.categoriesRepo.All
+                    .Where(x => x.Id == id.Value)
+                    .SingleOrDefault();
+
+                if (product != null)
+                {
+                    result = product;
+                }
+            }
+
+            return result;
+        }
+
+        public void AddCategory(Category category)
+        {
+            this.categoriesRepo.Add(category);
+            this.context.Commit();
+        }
+    }
+}
---
+++ IzgodnoKupi/IzgodnoKupi.Services/Contracts/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using IzgodnoKupi.Data.Model;
+using System;
+using System.Linq;
+
+namespace IzgodnoKupi.Services.Contracts
+{
+    public interface ICategoryService
+    {
+        IQueryable<Category> GetAllCategoriesSortedByName();
+
+        Category GetById(Guid? id);
+
+        void AddCategory(Category category);
+    }
+}
