0ab1f50701fab8270f5db488296adb9701b69d26
Camyul <vankata.lt@outlook.com>
2017-12-07 14:36:45 +0200

    Added OrderItemsService and Interrface

---
+++ IzgodnoKupi/IzgodnoKupi.Services/Contracts/IOrderItemsService.cs
@@ -0,0 +1,18 @@
+﻿using IzgodnoKupi.Data.Model;
+using System;
+
+namespace IzgodnoKupi.Services.Contracts
+{
+    public interface IOrderItemsService
+    {
+        // IEnumerable<OrderItem> GetAll();
+
+        OrderItem GetById(Guid? id);
+
+        Guid Update(OrderItem orderItem);
+
+        Guid Delete(OrderItem orderItem);
+
+        Guid Create(OrderItem orderItem);
+    }
+}
---
+++ IzgodnoKupi/IzgodnoKupi.Services/OrderItemsService.cs
@@ -0,0 +1,54 @@
+﻿using Bytes2you.Validation;
+using IzgodnoKupi.Data.Contracts;
+using IzgodnoKupi.Data.Model;
+using IzgodnoKupi.Services.Contracts;
+using System;
+
+namespace IzgodnoKupi.Services
+{
+    public class OrderItemsService : IOrderItemsService
+    {
+        private readonly IEfRepository<OrderItem> orderItemsRepo;
+        private readonly ISaveContext context;
+
+        public OrderItemsService(IEfRepository<OrderItem> orderItemsRepo, ISaveContext context)
+        {
+            Guard.WhenArgument(orderItemsRepo, "OrderItemsRepo").IsNull().Throw();
+            Guard.WhenArgument(context, "OrderItemscontext").IsNull().Throw();
+
+            this.orderItemsRepo = orderItemsRepo;
+            this.context = context;
+        }
+
+        public Guid Create(OrderItem orderItem)
+        {
+            this.orderItemsRepo.Add(orderItem);
+            context.Commit();
+
+            return orderItem.Id;
+        }
+
+        public Guid Delete(OrderItem orderItem)
+        {
+            OrderItem orderItemToDelete = this.orderItemsRepo.GetById(orderItem.Id);//???????????????
+            this.orderItemsRepo.Delete(orderItemToDelete);
+            this.context.Commit();
+
+            return orderItem.Id;
+        }
+
+        public OrderItem GetById(Guid? id)
+        {
+            return id.HasValue ? this.orderItemsRepo.GetById(id) : null;
+        }
+
+        public Guid Update(OrderItem orderItem)
+        {
+            OrderItem orderItemToUpdate = this.orderItemsRepo.GetById(orderItem.Id);
+            this.orderItemsRepo.Delete(orderItemToUpdate);
+            this.context.Commit();
+
+            return orderItem.Id;
+        }
+    }
+}
