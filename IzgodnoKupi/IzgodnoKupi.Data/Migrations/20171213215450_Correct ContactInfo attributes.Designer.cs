316b6f671c8afd7332efa49a273e7bcb5e28e285
Camyul <vankata.lt@outlook.com>
2017-12-14 00:02:39 +0200

    Added migration

--- IzgodnoKupi/IzgodnoKupi.Data/Migrations/20171213215450_Correct ContactInfo attributes.Designer.cs
+++ IzgodnoKupi/IzgodnoKupi.Data/Migrations/20171213215450_Correct ContactInfo attributes.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace IzgodnoKupi.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171213215450_Correct ContactInfo attributes")]
+    partial class CorrectContactInfoattributes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IzgodnoKupi/IzgodnoKupi.Data/Migrations/20171213215450_Correct ContactInfo attributes.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace IzgodnoKupi.Data.Migrations
+{
+    public partial class CorrectContactInfoattributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PostCode",
+                table: "FullContactInfo",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OfficeName",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MOL",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EIK",
+                table: "FullContactInfo",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyCity",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyAddress",
+                table: "FullContactInfo",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BGEIK",
+                table: "FullContactInfo",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Area",
+                table: "FullContactInfo",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PostCode",
+                table: "FullContactInfo",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OfficeName",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MOL",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EIK",
+                table: "FullContactInfo",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyCity",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyAddress",
+                table: "FullContactInfo",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BGEIK",
+                table: "FullContactInfo",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Area",
+                table: "FullContactInfo",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70);
+        }
+    }
+}
