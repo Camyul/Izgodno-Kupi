b3a59ced1e53f162c1354249eb2f6b9ad1bba2ae
Camyul <vankata.lt@outlook.com>
2018-01-18 00:46:16 +0200

    Get Categories from Stantek

---
+++ IzgodnoKupi/IzgodnoKupi/Areas/Admin/Controllers/StantekCrowlerController.cs
@@ -0,0 +1,81 @@
+﻿using Bytes2you.Validation;
+using HtmlAgilityPack;
+using IzgodnoKupi.Data.Model;
+using IzgodnoKupi.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using IzgodnoKupi.Web.Areas.Admin.Models.Category;
+
+namespace IzgodnoKupi.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class StantekCrowlerController : Controller
+    {
+        private readonly IProductsService productsService;
+        private readonly ICategoriesService categiriesService;
+
+        public StantekCrowlerController(IProductsService productsService, ICategoriesService categiriesService)
+        {
+            Guard.WhenArgument(productsService, "productsService").IsNull().Throw();
+            Guard.WhenArgument(categiriesService, "categiriesService").IsNull().Throw();
+
+            this.productsService = productsService;
+            this.categiriesService = categiriesService;
+        }
+
+        public async Task<IActionResult> GetProducts()
+        {
+            var url = "http://stantek.com/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            IList<CategoryStantekViewModel> categories = GetCategoriesToList(htmlDocument);
+
+            
+
+            return RedirectToAction("Index", "Home", new { area = "Admin" });
+        }
+
+        private IList<CategoryStantekViewModel> GetCategoriesToList(HtmlDocument htmlDocument)
+        {
+            var categoriesDiv = htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("categories"))
+                .FirstOrDefault();
+
+            IList<HtmlNode> categoriesLi = categoriesDiv.Descendants("li").ToList();
+
+            IList<CategoryStantekViewModel> categories = new List<CategoryStantekViewModel>();
+
+            foreach (var li in categoriesLi)
+            {
+                if (li.Descendants("a").FirstOrDefault() == null)
+                {
+                    continue;
+                }
+
+                var catModel = new CategoryStantekViewModel()
+                {
+                    Name = li.Descendants("a").FirstOrDefault().InnerText,
+                    CategoryUrl = li.Descendants("a").FirstOrDefault()
+                                    .ChildAttributes("href").FirstOrDefault()
+                                    .Value
+
+
+                };
+                categories.Add(catModel);
+            }
+
+            return categories;
+        }
+    }
+}
---
+++ IzgodnoKupi/IzgodnoKupi/Areas/Admin/Models/Category/CategoryStantekViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IzgodnoKupi.Web.Areas.Admin.Models.Category
+{
+    public class CategoryStantekViewModel
+    {
+        public string Name { get; set; }
+
+        public string CategoryUrl { get; set; }
+    }
+}
